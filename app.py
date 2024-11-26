293ddd44f9fc2cddf42976f4f3ce706b18a9d073
Hernan Di Tano <hditano@gmail.com>
2024-11-27 00:09:36 +0100

    added db check for duplicates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import sqlite3
+import os
 
 r = requests.get('https://robertsspaceindustries.com/community/devtracker')
 
@@ -9,6 +11,14 @@ soup = BeautifulSoup(r.text, 'html.parser')
 time = soup.find_all('div', class_='devtracker-list')
 
 devposts = {}
+
+
+
+con = sqlite3.connect('database.db')
+
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS tracker(topic, category, url, post_time)")
 
 for tracker in time:
     topic = []
@@ -35,5 +45,28 @@ for tracker in time:
             'post_time': post_time,
         }
 
-for k, v in devposts.items():
-    print(f'{k} {v}\n')
+        existing_urls = [url[0] for url in cur.execute("SELECT url FROM tracker").fetchall()]
+
+
+        if devposts[ran]['urls'] in existing_urls:
+            print(f'URL already exists {existing_urls}')
+        else:
+            print(f'Adding url {devposts[ran]['urls']}')
+            cur.execute("INSERT INTO tracker VALUES (?, ?, ?, ?)", (devposts[ran]['topic'], devposts[ran]['category'], devposts[ran]['urls'], devposts[ran]['post_time']))
+            con.commit()
+
+
+# for item in existing_urls:
+#     if temp_url in item:
+#         cur.execute("DELETE FROM tracker WHERE url = ?", (temp_url,))
+#         con.commit()
+#         print(f'{temp_url} deleted')
+
+# for i in existing_urls:
+#     if temp_url in i:
+#         print(f'Value {temp_url} exists')
+#         cur.execute("DELETE FROM tracker WHERE url = ?", (temp_url,))
+#         con.commit()
+#         print(f'Deleted')
+#     else:
+#         print(f'Doesnt exist')
